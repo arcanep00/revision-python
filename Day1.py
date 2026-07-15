dec33f71b018661fa59a412cb2aa1d58378839f0
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-15 22:45:18 +0530

    Right aligned triangle

--- Day1.py
+++ Day1.py
@@ -1,8 +1,18 @@
 # operator precedence mai PEMDAS rule ko follow kiya jata hai
 
+# n = 5
+#
+# for i in range(1, n):
+#     for j in range(1, i + 1):
+#         print("*", end=" ")
+#     print()
+
+
 n = 5
 
-for i in range(1, n):
-    for j in range():
-        print("*", end=" ")
+for i in range(1, n + 1):
+    for j in range(n - i):
+        print(' ', end='')
+    for k in range(i):
+        print("*", end='')
     print()
