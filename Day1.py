ecb2e7ae20d26326052a1cf987e1992db71b49b5
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-16 23:09:34 +0530

    new changes

--- Day1.py
+++ Day1.py
@@ -7,10 +7,9 @@
 #         print("*", end=" ")
 #     print()
 
-
 n = 5
 
-for i in range(1, n + 1):
+for i in range(1, n):
     for j in range(n - i):
         print(' ', end='')
     for k in range(i):
