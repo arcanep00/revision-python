01891610115cf752a948c851538769ff8630f48f
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-16 23:49:48 +0530

    Add reverse words solution

---
+++ Strings.py
@@ -0,0 +1,73 @@
+# number pattern
+
+# n = 5
+# for i in range(1, n):
+#     for j in range(1, i + 1):
+#         print(j , end=" ")
+#     print()
+#
+#
+
+# s = "Python"
+#
+# s = "J" + s[1:]
+#
+# print(s)
+#
+
+
+# s = "I like JAVA"
+# print(s.replace("JAVA", "PYTHON"))
+
+# s = "Python programming"
+#
+# print(s.find("pro"))
+# print(s.count("o"))
+
+# s = "Python is easy"
+#
+# print(s.split())
+#
+
+"""
+Problem 1: Reverse words
+"""
+
+# def reverse_words(s):
+#     result = ""
+#     words = s.split()
+#
+#     for i in range(len(words) - 1, -1, -1):
+#         result += words[i]
+#
+#         if i != 0:
+#             result += " "
+#
+#     return result
+#
+# print(reverse_words("Python is awesome"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ patterns.py
@@ -0,0 +1,19 @@
+# operator precedence mai PEMDAS rule ko follow kiya jata hai
+
+# n = 5
+#
+# for i in range(1, n):
+#     for j in range(1, i + 1):
+#         print("*", end=" ")
+#     print()
+
+# Right aligned triangle
+
+# n = 5
+
+# for i in range(1, n):
+#     for j in range(n - i):
+#         print(' ', end='')
+#     for k in range(i):
+#         print("*", end='')
+#     print()
