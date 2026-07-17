a32996e5399320e7522e3019842249fbe61faad6
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-17 17:15:26 +0530

    add string compression solution

--- Strings.py
+++ Strings.py
@@ -68,9 +68,26 @@ Question 2: First Non-Repeating Character
 
 # Question 3: String Compression
 
-def compress_string(s):
-    result = ""
-    for ch in s:
+# def compress_string(s):
+#     if not s:
+#         return ""
+#
+#     result = ""
+#     count = 1
+#
+#     for i in range(1, len(s)):
+#         if s[i] == s[i - 1]:
+#             count += 1
+#         else:
+#             result += s[i - 1] + str(count)
+#             count = 1
+#
+#     result += s[-1] + str(count)
+#     return result
+#
+# print(compress_string("aaabbcccdaa"))
+
+
 
 
 
