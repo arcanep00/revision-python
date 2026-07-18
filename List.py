17e7dc76c74040915b473b7c3879d99f54ed7e3f
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-18 14:43:53 +0530

    add solution of remove duplicates

--- List.py
+++ List.py
@@ -2,19 +2,42 @@
 List
 """
 
-def second_largest_element(lst):
-    first = float('-inf')
-    second = float('-inf')
+"""
+second largest element - 
+"""
 
-    for num in lst:
-        if num > first:
-            second = first
-            first = num
+# def second_largest_element(lst):
+#     first = float('-inf')
+#     second = float('-inf')
+#
+#     for num in lst:
+#         if num > first:
+#             second = first
+#             first = num
+#
+#         elif num > second and num != first:
+#             second = num
+#     return second
+#
+# print(second_largest_element([10, 20, 5, 8, 15]))
 
-        elif num > second and num != first:
-            second = num
-    return second
+"""
+remove duplicates - 
+"""
 
-print(second_largest_element([10, 20, 5, 8, 15]))
+# def remove_duplicates(lst):
+#     seen = set()
+#     result = []
+#     for item in lst:
+#         if item not in seen:
+#             seen.add(item)
+#             lst.append(item)
+#
+#     return lst
+#
+# print(remove_duplicates([1, 2, 2, 3, 1, 4, 5, 4]))
+
+
+
 
 
