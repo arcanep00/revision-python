07bbd702bacf64febb29389757e928c9f47f319d
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-18 14:08:12 +0530

    add hollow square with center star pattern

--- patterns.py
+++ patterns.py
@@ -17,3 +17,25 @@
 #     for k in range(i):
 #         print("*", end='')
 #     print()
+
+"""
+Hollow square with center star -
+
+* * * * *
+*       *
+*   *   *
+*       *
+* * * * *
+
+"""
+# n = 5
+#
+# for i in range(1, n + 1):
+#     for j in range(1, n + 1):
+#         if i == 1 or i == n or j == 1 or j == n or (i == 3 and j == 3):
+#             print("*", end=" ")
+#         else:
+#             print(' ', end=" ")
+#     print()
+
+
