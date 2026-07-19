7c983f711255bcf9e66c07b471abe7c714f977a6
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-19 20:29:03 +0530

    Add pyramid pattern

--- patterns.py
+++ patterns.py
@@ -38,4 +38,44 @@ Hollow square with center star -
 #             print(' ', end=" ")
 #     print()
 
+"""
+Pyramid -
+
+    *
+   ***
+  *****
+ *******
+*********
+
+"""
+
+# n = 5
+#
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for j in range(2 * i + 1):
+#         print("*", end="")
+#     print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
