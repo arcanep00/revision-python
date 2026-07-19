88072dd753877c98e24fc36093cc86aae3c08ffb
Abhinav Pandey <navabhiday00@gmail.com>
2026-07-19 20:38:09 +0530

    add find missing number solution

--- List.py
+++ List.py
@@ -53,6 +53,39 @@ rotate list
 #
 # print(rotate_list([1, 2, 3, 4, 5], 2))
 
+"""
+Find missing number -
+"""
+
+# def find_missing_number(lst):
+#     n = len(lst) + 1
+#
+#     expected_sum = n * (n + 1) // 2
+#     actual_sum = sum(lst)
+#
+#     return expected_sum - actual_sum
+#
+# print(find_missing_number([1, 2, 3, 4, 6]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
